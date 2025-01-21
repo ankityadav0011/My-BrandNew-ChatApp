ffe5d1080217f980c9cd4af7c7d451a2c063f6b7
Ankit Kumar <144920510+ankityadav0011@users.noreply.github.com>
2025-01-21 23:06:29 +0530

    Update index.js

--- socket/index.js
+++ socket/index.js
@@ -12,7 +12,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
     cors: {
-        origin: 'https://famous-dolphin-b4f33e.netlify.app',
+        origin: 'https://sensational-toffee-70db83.netlify.app',
         credentials: true
     }
 });
