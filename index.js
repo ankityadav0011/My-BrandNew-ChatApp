4197736ae24f328b3af1cd284a062548ded91b58
Ankit Kumar <ankitkumar7208701@gmail.com>
2025-01-21 19:51:24 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,35 @@
+const express = require('express')
+const cors = require('cors')
+require('dotenv').config()
+const connectDB = require("./config/connectDB")
+const router = require("./routes/index")
+const cookiesParser = require("cookie-parser")
+
+const {app,server} = require("./socket/index")
+
+
+
+app.use(cors({
+    origin:process.env.FRONTEND_URL,
+    credentials:true,
+}))
+
+const PORT = process.env.PORT || 5050;
+
+app.get('/',(req,res)=>{
+    res.json({
+        message:`server is running on port ${PORT}`
+    })
+})
+
+app.use(express.json())
+app.use(cookiesParser())
+// APi Endpoints 
+app.use("/api",router);
+
+connectDB().then(()=>{
+    console.log("Db Connected");
+})
+server.listen(PORT,()=>{
+    console.log("SERVer is runnning" + `localhost:${PORT}`)
+})
