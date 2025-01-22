a525e36fa561100ab4e9d851cc88ca6238300b36
Ankit Kumar <144920510+ankityadav0011@users.noreply.github.com>
2025-01-22 12:24:08 +0530

    Update index.js

--- socket/index.js
+++ socket/index.js
@@ -12,7 +12,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
     cors: {
-        origin:'*',
+        origin:'https://sensational-toffee-70db83.netlify.app',
         credentials: true
     },
     pingTimeout: 60000,  // Increase timeout to 1 minute (or more)
